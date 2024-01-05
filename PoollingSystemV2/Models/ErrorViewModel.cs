b4df991e3f19cfdc2cef4ebbb10b75cb5eb852cd
Kennedy <kennedy@bitspacedevelopment.com>
2024-01-05 10:28:23 -0600

    Initial project setup

---
+++ PoollingSystemV2/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PollingSystemMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
